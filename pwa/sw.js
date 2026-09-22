07e389d42aef7f2db67fbbd574da7aa3a3bde97b
ohky1008 <152399362+ohky1008@users.noreply.github.com>
2026-09-22 20:40:30 +0900

    Add files via upload

---
+++ pwa/sw.js
@@ -0,0 +1,48 @@
+const CACHE = 'lecture-translate-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './support.js',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-512.png',
+  './apple-touch-icon.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  // Never cache translation API calls
+  if (url.origin !== self.location.origin) return;
+  e.respondWith(
+    caches.match(req).then((hit) => {
+      if (hit) {
+        fetch(req).then((res) => {
+          if (res && res.ok) caches.open(CACHE).then((c) => c.put(req, res.clone()));
+        }).catch(() => {});
+        return hit;
+      }
+      return fetch(req).then((res) => {
+        if (res && res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(req, copy));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
